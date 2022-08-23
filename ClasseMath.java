3b939c3bab0b8bd47f4be8c025b104eaf9f294f3
rafael <89603873+raffaelcv@users.noreply.github.com>
2022-08-23 10:08:04 -0300

    Add files via upload

---
+++ ClasseMath.java
@@ -0,0 +1,24 @@
+
+public class ClasseMath {
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		
+		//compra e retorna o maior numero de dois numeros
+		System.out.println("O maior número entre 9 e 2 é: " + Math.max(9, 2));
+		
+		//retorna o menos entre dois numeros
+		System.out.println("O maior número entre 9 e 2 é: " + Math.min(1, 9));
+
+		
+		System.out.println("A raiz quadrada de 9 é : " + Math.sqrt(81));
+		System.out.println("O numero aleatorio gerado é : " + Math.random());
+		
+		//Round - arredonda numeros
+		System.out.println("O arrendondamento de 7.8 é : " + Math.round(7.8));
+		System.out.println("O arrendondamento de 7.4 é: " + Math.round(7.4));
+
+		
+	}
+
+}
